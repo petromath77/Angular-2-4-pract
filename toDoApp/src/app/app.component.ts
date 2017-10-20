f017c95d226159d5bc54be1f8789e53941ff8984
petromath77 <petromath77@gmail.com>
2017-10-20 17:31:13 +0300

    todoApp/add_list

--- toDoApp/src/app/app.component.ts
+++ toDoApp/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+class Todo{
+  constructor(public title: string, public completed: boolean = false){
+  }
+}
+
 const todos=[
-  'Изучить Angular2',
-  'Изучить Javascript',
-  'Написат приложение',
-  'Захватить Вселенную'
+  {
+    title: 'Изучить Angular2',
+    completed: true
+  },
+  {
+    title: 'Изучить Javascript',
+    completed: false
+  },
+  {
+    title: 'Написат приложение',
+    completed: false
+  },
+  {
+    title: 'Захватить Вселенную',
+    completed: false
+  }
+  
 ]
 
 @Component({
@@ -14,5 +32,21 @@ const todos=[
 })
 export class AppComponent {
   title = 'toDoApp';
-  todos = todos;
+  todos: Todo[]= todos;
+
+  toggle(todo: Todo){
+   todo.completed = !todo.completed;
+  }
+  delete(todo: Todo){
+    let index = this.todos.indexOf(todo);
+
+    if(index > -1){
+      this.todos.splice(index, 1);
+    }
+  }
+  create(event: Event, title: string){
+    event.preventDefault();
+    let todo: Todo = new Todo(title);
+    this.todos.push(todo);
+  }
 }
